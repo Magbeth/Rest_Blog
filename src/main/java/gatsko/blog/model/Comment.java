5cc6fc0a75e7be450c1923e2e281566f440f4bb9
Vladislav Gatsko <magbethlmc@gmail.com>
2019-04-09 13:35:13 +0300

    some code cleaning

--- src/main/java/gatsko/blog/model/Comment.java
+++ src/main/java/gatsko/blog/model/Comment.java
@@ -26,17 +26,13 @@ public class Comment {
     @Column(name = "message", columnDefinition = "text", nullable = false)
     private String commentText;
 
-    @JsonSerialize(using = JsonDateSerializer.class)
-    @JsonDeserialize(using = JsonDateDeserializer.class)
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "author_id", nullable = false)
     private User user;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id", nullable = false)
     private Article article;
